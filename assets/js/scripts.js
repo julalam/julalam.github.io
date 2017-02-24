6df63d9f0084bc5397f56077d9cb1014157d7a80
julalam <julia.chikulaeva@gmail.com>
2017-02-23 19:11:46 -0800

    Family Website

---
+++ assets/js/scripts.js
@@ -0,0 +1,114 @@
+
+jQuery(document).ready(function() {
+	
+    /*
+        Fullscreen background
+    */
+    //$.backstretch("assets/img/backgrounds/home-bg-01.jpg");
+    
+    $('#top-navbar-1').on('shown.bs.collapse', function(){
+    	$.backstretch("resize");
+    });
+    $('#top-navbar-1').on('hidden.bs.collapse', function(){
+    	$.backstretch("resize");
+    });
+    
+    /*
+	    Contact form
+	*/
+	$('.contact-form form input[type="text"], .contact-form form textarea').on('focus', function() {
+		$('.contact-form form input[type="text"], .contact-form form textarea').removeClass('input-error');
+	});
+	$('.contact-form form').submit(function(e) {
+		e.preventDefault();
+	    $('.contact-form form input[type="text"], .contact-form form textarea').removeClass('input-error');
+	    var postdata = $('.contact-form form').serialize();
+	    $.ajax({
+	        type: 'POST',
+	        url: 'assets/contact.php',
+	        data: postdata,
+	        dataType: 'json',
+	        success: function(json) {
+	            if(json.emailMessage != '') {
+	                $('.contact-form form .contact-email').addClass('input-error');
+	            }
+	            if(json.subjectMessage != '') {
+	                $('.contact-form form .contact-subject').addClass('input-error');
+	            }
+	            if(json.messageMessage != '') {
+	                $('.contact-form form textarea').addClass('input-error');
+	            }
+	            if(json.emailMessage == '' && json.subjectMessage == '' && json.messageMessage == '') {
+	                $('.contact-form form').fadeOut('fast', function() {
+	                    $('.contact-form').append('<p>Thanks for contacting us! We will get back to you very soon.</p>');
+	                    // reload background
+	    				$.backstretch("resize");
+	                });
+	            }
+	        }
+	    });
+	});
+    
+    
+});
+
+//kids gallery
+
+
+jQuery(document).ready(function ($) {
+
+    $('#myCarousel').carousel({
+        interval: 5000
+    });
+
+    //Handles the carousel thumbnails
+    $('[id^=carousel-selector-]').click(function () {
+        var id_selector = $(this).attr("id");
+        try {
+            var id = /-(\d+)$/.exec(id_selector)[1];
+            console.log(id_selector, id);
+            jQuery('#myCarousel').carousel(parseInt(id));
+        } catch (e) {
+            console.log('Regex failed!', e);
+        }
+    });
+    // When the carousel slides, auto update the text
+    $('#myCarousel').on('slid.bs.carousel', function (e) {
+        var id = $('.item.active').data('slide-number');
+        $('#carousel-text').html($('#slide-content-' + id).html());
+    });
+});
+
+//hobby gallery 
+
+jQuery(document).ready(function ($) {
+
+    $('#myCarousel').carousel({
+        interval: 5000
+    });
+
+    $('#carousel-text').html($('#slide-content-0').html());
+
+    //Handles the carousel thumbnails
+    $('[id^=carousel-selector-]').click(function () {
+        var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+        var id = parseInt(id);
+        $('#myCarousel').carousel(id);
+    });
+
+
+    // When the carousel slides, auto update the text
+    $('#myCarousel').on('slid.bs.carousel', function (e) {
+        var id = $('.item.active').data('slide-number');
+        $('#carousel-text').html($('#slide-content-' + id).html());
+    });
+});
+
+//login
+
+$(document).ready(function () {
+    //Handles menu drop down
+    $('.dropdown-menu').find('form').click(function (e) {
+        e.stopPropagation();
+    });
+});
